c410d587921e33599b0500c939c658c3a79754a1
Karsten König <KarstenKoenig@gmx.net>
2010-08-19 19:02:52 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name = "simpleRenamer",
+	description = "A simple renamer written in Python with some nice options.",
+	long_description = "A simple renamer written in Python. simpleRenamer will rename the files of a directory sequentially, that is they will be numbered serially.",
+	version = "v.1.0",
+	license = "MIT-License",
+	author = "Karsten Koenig",
+	author_email = "KarstenKoenig@gmx.net",
+	url = 'http://www.github.com/raymontag/simpleRenamer',
+	download_url = 'http://www.github.com/raymontag/simpleRenamer/simpleRenamer-v.1.0.tar.gz',
+	scripts = ['src/simplerenamer'],
+	platforms = "Linux"
+)
